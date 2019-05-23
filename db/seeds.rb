43b0e355f6b4cb6e6552c135edbd0a2bd0029ab9
Tina Liu <tina.chang.liu@gmail.com>
2019-05-23 23:51:12 +0900

    cocktails, doses and ingredients model added, working fornt end

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+
+puts 'Cleaning database...'
+
+Ingredient.destroy_all
+Dose.destroy_all
+Cocktail.destroy_all
+
+puts 'creating ingredients'
+
+response = open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list')
+json = JSON.parse(response.read)
+max_i = json['drinks'].size
+
+(0..max_i).each do |i|
+  Ingredient.create!(name: json['drinks'][i]['strIngredient1'])
+end
+
+puts 'creating 1 cocktail and 1 dose'
+
+@cocktail = Cocktail.create!(name: "Thyme Delight")
+
+@dose1 = Dose.new(description: "40g")
+@ice = Ingredient.find_by(name: "Ice")
+@dose1.cocktail = @cocktail
+@dose1.ingredient = @ice
+@dose1.save
+
+puts 'finished'
